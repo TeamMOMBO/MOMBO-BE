97f9f4d5879a29a19e24af8f4344268cfc054a25
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2024-10-12 13:34:31 +0900

    feat : User Serializers

---
+++ user/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import User, Profile
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email = validated_data['email'],
+            password = validated_data['password'],
+            )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(source='user.email', read_only=True)
+    class Meta:
+        model = Profile
+        fields = ['id', 'nickname', 'pregnancy_date', 'email']
