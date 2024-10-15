b7ce462d00d36edbb602357fa5fd1818f42e5b13
Woohyun <gusdn0481@gmail.com>
2024-10-15 20:46:30 +0900

    feat : pregnancy url 및 model 정의

--- pregnancy/models.py
+++ pregnancy/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class FAQ(models.Model):
+    question = models.CharField(null=True, blank=True)
+    real_question = models.CharField(null=True, blank=True)
+    answer = models.CharField(null=True, blank=True)
+    views = models.IntegerField(null=True, blank=True)
+
+
+class Information(models.Model):
+    step = models.CharField(null=True, blank=True)
+    Week = models.IntegerField(null=True, blank=True)
+    fetus = models.CharField(null=True, blank=True)
+    maternity = models.CharField(null=True, blank=True)
+    summary = models.CharField(null=True, blank=True)
+
