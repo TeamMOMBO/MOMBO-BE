a168e263a0012fe6074d611ed4ff700ca2b775c5
Woohyun <gusdn0481@gmail.com>
2024-10-15 11:20:13 +0900

    feat : admin 모델 추가

--- user/urls.py
+++ user/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import Login, Join, Withdrawal, Logout, KakaoLogin, ProfileView, ProfileEditView
+from .views import Login, Join, Withdrawal, Logout, ProfileView, ProfileEditView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.contrib.auth import views as auth_views
 
 app_name = 'user'
 
 urlpatterns = [
-    path('login/', Login.as_view(), name='login'),
+    # path('login/', Login.as_view(), name='login'),
     path('join/', Join.as_view(), name='join'),
     path("logout/", Logout.as_view(), name='logout'),
     path("withdrawal/", Withdrawal.as_view(), name='withdrawal'),
-    path("login/kakao/callback/", KakaoLogin.as_view(), name='kakao-callback'),
+    path("login/kakao/callback/", Login.as_view(), name='kakao-callback'),
     path("profile/", ProfileView.as_view(), name='profile'),
     path("profile/edit/", ProfileEditView.as_view(), name='profile-edit'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
