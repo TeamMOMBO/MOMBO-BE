46f022fca48b8700d3b15babde878abba78d3280
Hyunwooz <gusdn0481@gmail.com>
2024-10-20 16:59:19 +0900

    fix : s3 upload 모듈 수정

--- ingredient/imgUpload.py
+++ ingredient/imgUpload.py
@@ -1,31 +1,25 @@
 from dotenv import load_dotenv
+import mimetypes
+from io import BytesIO
 import boto3
 import uuid
 import os
+from PIL import Image
 
 load_dotenv()
-
 
 class S3ImgUploader:
     def __init__(self, file):
         self.file = file
 
     def upload(self, folder):
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-        )
+        
         url = folder+'/'+uuid.uuid1().hex
-        s3_client.upload_fileobj(
-            self.file, 
-            os.environ.get("AWS_STORAGE_BUCKET_NAME"), 
-            url, 
-            ExtraArgs={
-                "ContentType": self.file.content_type
-            }
-        )
+        s3r = boto3.resource('s3', aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
+        s3r.Bucket(os.environ.get("AWS_STORAGE_BUCKET_NAME")).put_object(Key=url, Body=self.file, ContentType='jpg')
+        
         return url
+
     def delete(self):
         s3_client = boto3.client(
             's3',
@@ -34,6 +28,7 @@ class S3ImgUploader:
         )
 
         try:
+            # S3 객체 삭제
             s3_client.delete_object(
                 Bucket=os.environ.get("AWS_STORAGE_BUCKET_NAME"),
                 Key=str(self.file)  
