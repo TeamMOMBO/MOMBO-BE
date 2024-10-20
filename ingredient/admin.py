6a75133a64da0a35a4cb274b03a2a46204f9fe7d
Hyunwooz <gusdn0481@gmail.com>
2024-10-20 16:58:59 +0900

    feat : ingredient admin 추가

--- ingredient/admin.py
+++ ingredient/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ingredient, UserAnalysisResult, IngredientResult
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(UserAnalysisResult)
+admin.site.register(IngredientResult)
