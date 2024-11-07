e758597274fc9d7e95f43d3516beac663ab8da4b
Woohyun <gusdn0481@gmail.com>
2024-11-07 18:44:52 +0900

    feat : search 수정

--- pregnancy/views.py
+++ pregnancy/views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import OpenApiExample, extend_schema, inline_serializer, OpenApiParameter
+from drf_spectacular.utils import OpenApiExample, extend_schema, inline_serializer, OpenApiParameter, OpenApiResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.views import APIView
@@ -128,78 +128,108 @@ class Home(APIView):
 class Search(APIView):
     @extend_schema(
         summary="검색 결과 API",
-        description="검색 결과 API에 대한 설명 입니다.",
-        parameters=[],
-        tags=["search"],
-        responses=InformationSerializer,
-        examples=[
-            OpenApiExample(
-                response_only=True,
-                name="200_OK",
-                value={
-                        "faqs": [
-                            {
-                                "id": 114,
-                                "question": "임신 중 배 뭉침이 너무 잦아서 걱정입니다.",
-                                "real_question": "임신 29주 때 배 뭉침이 잦아서 문의 드렸는데, ...",
-                                "answer": "자궁 경부 길이가 짧아졌다는 것은 조기 진통 ...",
-                                "views": 0
-                            },
-                            {
-                                "id": 166,
-                                "question": "[모유수유와 생리] 모유수유중에는 생리를 안하나요?",
-                                "real_question": "아이가 돌이 지나 곧 수유를 중단하려고 ..",
-                                "answer": "모유수유중인 엄마에서도 분만 3주 후 부터는 모유수는 ...",
-                                "views": 0
-                            },
-                            {
-                                "id": 212,
-                                "question": "[2차성 불임증] 출산한지 2년이 넘었는데 둘째 아이가 안 생겨요",
-                                "real_question": "저는 첫째아이를 출산한지 2년이 넘었습니다. ...",
-                                "answer": "처음부터 아예 임신이 되지 않은 것이 아니고 ...",
-                                "views": 0
-                            }
-                        ]
-                    }
-            ),
-            OpenApiExample(
-                response_only=True,
-                name="400_BAD_REQUEST",
-                value={
-                    "message": "키워드가 존재하지 않습니다.",
-                },
-            ),
-            OpenApiExample(
-                response_only=True,
-                name="401_UNAUTHORIZED",
-                value={
-                    "message": "401_UNAUTHORIZED",
-                },
-            ),
+        description="검색 결과 API에 대한 설명 입니다. 주어진 키워드에 따라 FAQ와 성분 정보를 검색하여 반환합니다.",
+        parameters=[
+            OpenApiParameter(name='keyword', description='검색어', required=True, type=str),
+            OpenApiParameter(name='page', description='페이지 번호 (정수 값)', required=False, type=int),
+            OpenApiParameter(
+                name='category', 
+                description="검색 카테고리 (가능한 값: 'all', 'content', 'ingredient')", 
+                required=False, 
+                type=str,
+                enum=['all', 'content', 'ingredient']  # 선택 가능한 값 제한
+            )
         ],
+        tags=["Search"],
+        responses={
+            200: OpenApiResponse(
+                response=InformationSerializer,
+                description="검색 결과를 반환합니다.",
+                examples=[
+                    OpenApiExample(
+                        name="200_OK",
+                        value={
+                            "faqs": [
+                                {
+                                    "id": 114,
+                                    "question": "임신 중 배 뭉침이 너무 잦아서 걱정입니다.",
+                                    "real_question": "임신 29주 때 배 뭉침이 잦아서 문의 드렸는데, ...",
+                                    "answer": "자궁 경부 길이가 짧아졌다는 것은 조기 진통 ...",
+                                    "views": 0
+                                }
+                            ],
+                            "ingredients": [
+                                {
+                                    "id": 1,
+                                    "ingredientKr": "성분 예시",
+                                    "ingredientDescription": "이 성분은 예시 설명입니다."
+                                }
+                            ]
+                        },
+                    ),
+                    OpenApiExample(
+                        name="400_BAD_REQUEST",
+                        value={
+                            "message": "검색 창이 입력되지 않았습니다.",
+                        },
+                    ),
+                    OpenApiExample(
+                        name="401_UNAUTHORIZED",
+                        value={
+                            "message": "401_UNAUTHORIZED",
+                        },
+                    ),
+                ],
+            )
+        },
     )
     def get(self, request):
         keyword = request.GET.get('keyword')
+        category = request.GET.get('category')
+        page = int(request.GET.get('page', 1))
 
         if not keyword:
-            return Response({"message":"검색 창이 입력되지 않았습니다."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "검색 창이 입력되지 않았습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-        faqs = FAQ.objects.filter(Q(question__icontains=keyword) | Q(real_question__icontains=keyword)| Q(answer__icontains=keyword)).order_by('-id')
-        faqs_serializer = FAQSerializer(faqs, many=True).data
-        
-        ingredients = Ingredient.objects.filter(Q(ingredientKr__icontains=keyword)).order_by('-id')
-        ingredients_serializer = IngredientSerializer(ingredients, many=True).data
-        
-        response_data = {
-            "faqs" : faqs_serializer,
-            "ingredients" : ingredients_serializer,
-        }
+        if category == 'all':
+            faqs = FAQ.objects.filter(
+                Q(question__icontains=keyword) | Q(real_question__icontains=keyword) | Q(answer__icontains=keyword)
+            ).order_by('-id')[:3]
+            faqs_serializer = FAQSerializer(faqs, many=True).data
+            
+            ingredients = Ingredient.objects.filter(Q(ingredientKr__icontains=keyword)).order_by('-id')[:3]
+            ingredients_serializer = IngredientSerializer(ingredients, many=True).data
+            
+            response_data = {
+                "faqs": faqs_serializer,
+                "ingredients": ingredients_serializer,
+            }
 
-        return Response(response_data, status=status.HTTP_200_OK)
+            return Response(response_data, status=status.HTTP_200_OK)
+        elif category == 'content':
+            faqs = FAQ.objects.filter(
+                Q(question__icontains=keyword) | Q(real_question__icontains=keyword) | Q(answer__icontains=keyword)
+            ).order_by('-id')
+            faqs_serializer = FAQSerializer(faqs, many=True).data
+            
+            response_data = {
+                "faqs": faqs_serializer,
+            }
 
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            ingredients = Ingredient.objects.filter(Q(ingredientKr__icontains=keyword)).order_by('-id')
+            ingredients_serializer = IngredientSerializer(ingredients, many=True).data
+            
+            response_data = {
+                "ingredients": ingredients_serializer,
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
 
 class FAQUploadAPIView(APIView):
     parser_classes = (MultiPartParser, FormParser)
+    @extend_schema(exclude=True)
     def post(self, request, *args, **kwargs):
         file = request.FILES.get('file')
 
@@ -231,6 +261,7 @@ class FAQUploadAPIView(APIView):
 
 class InfomationUploadAPIView(APIView):
     parser_classes = (MultiPartParser, FormParser)
+    @extend_schema(exclude=True)
     def post(self, request, *args, **kwargs):
         file = request.FILES.get('file')
 
