45bb3c02f08d802a1e73fbb4c5ef5cad8b637f47
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2024-12-06 21:30:17 +0900

    test : Home API Test

--- pregnancy/views.py
+++ pregnancy/views.py
@@ -96,13 +96,13 @@ class Home(APIView):
     def get(self, request):
         
         try:
-            user = request.user
-            # user = User.objects.get(id=1)
+            # user = request.user
+            user = User.objects.get(id=request.user)
         except:
             Response({'message':'401_UNAUTHORIZED'}, status=status.HTTP_401_UNAUTHORIZED)
 
         # 프로필 가져오기
-        profile = Profile.objects.get(user=user.id)
+        profile = Profile.objects.get(user=user)
         profileSerializer = ProfileSerializer(profile) # 프로필 직렬화
 
         # 주차별 정보 가져오기 / pregnancyDate로 주차 계산
