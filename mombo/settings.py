9887a5c25f62c7efd51387d1bed4250c1bacb985
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2024-12-18 21:45:08 +0900

    test : jwt 인증 변경경

--- mombo/settings.py
+++ mombo/settings.py
@@ -75,8 +75,8 @@ REST_FRAMEWORK = {
     ],
     'NON_FIELD_ERRORS_KEY':'errors',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'mombo.authentication.CookieJWTAuthentication'
     ),
     
